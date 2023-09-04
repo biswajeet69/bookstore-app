94570c2ab502297c77113cc1533e0b78724bb465
biswajeet <119700255+biswajeet69@users.noreply.github.com>
2023-09-05 01:46:58 +0530

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,3 @@
 export const PORT = 5555;
 
-//XccFBb5S3KZcz8eo
-
 export const mongoDBURL = 'mongodb+srv://book:XccFBb5S3KZcz8eo@book-store.mgwy72a.mongodb.net/book-collection?retryWrites=true&w=majority'
