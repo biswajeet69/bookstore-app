f247ae02cd8c80517b8a3f60f2c1b8d252248955
biswajeet <119700255+biswajeet69@users.noreply.github.com>
2023-09-07 23:46:55 +0530

    created pages

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const [books, setBooks] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [showType, setShowType] = useState('table');
 
-export default Home
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get('http://localhost:5555/books')
+      .then((response) => {
+        setBooks(response.data.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setLoading(false);
+      });
+  }, []);
+
+
+
+export default Home;
