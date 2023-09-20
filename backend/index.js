6fd1a26ffdd0e668a4cb1dc0cca1504bf4215593
biswajeet <119700255+biswajeet69@users.noreply.github.com>
2023-09-20 20:32:16 +0530

    added edit and create functionality

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,16 @@ import express, { response } from 'express';
 import { PORT, mongoDBURL } from './config.js';
 import mongoose from 'mongoose';
 import booksRoute from './routes/booksRoute.js';
+import cors from 'cors';
 
 const app = express();
 
 // Middleware for parsing request body
 app.use(express.json());
+
+// Middleware for handling CORS POLICY
+// Option 1: Allow All Origins with Default of cors(*)
+app.use(cors());
 
 app.get('/', (request, response) => {
     console.log(request);
